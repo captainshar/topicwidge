e3f5de22cd6fa9b17458d228a5c9ebae5bc9e3d1
Sharon C <captainshar@gmail.com>
2016-10-30 16:42:05 -0400

    added the view for "show" for the CommunitiesController

--- app/controllers/api/communities_controller.rb
+++ app/controllers/api/communities_controller.rb
@@ -4,6 +4,10 @@ class Api::CommunitiesController < ApplicationController
   	# shows the search term
   	# with the url in the style http://localhost:3000/api/community.json?term=howdy
   	# TODO: Scrape the community page and return the correct JSON for the search, using Nokogiri
-    render json: { term: params["term"] }
+#    render json: { term: params["term"] }
+    require 'open-uri'
+    @doc = Nokogiri::HTML(open("https://www.digitalocean.com/community/search?q=coreos"))
+    @articles = @doc.css("div.feedable-details a")
+#    puts articles
   end
 end
