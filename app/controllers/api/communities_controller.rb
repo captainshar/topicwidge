5867cf1c6ba2975584900db7886907aa8dd1ff94
Sharon C <captainshar@gmail.com>
2016-11-16 22:37:17 -0500

    Successfully added iframe for google trends
    Still need to combine it with the home.js file so it’s not in the html
    file

--- app/controllers/api/communities_controller.rb
+++ app/controllers/api/communities_controller.rb
@@ -6,8 +6,17 @@ class Api::CommunitiesController < ApplicationController
   	# TODO: Scrape the community page and return the correct JSON for the search, using Nokogiri
 #    render json: { term: params["term"] }
     require 'open-uri'
-    @doc = Nokogiri::HTML(open("https://www.digitalocean.com/community/search?q=coreos"))
-    @articles = @doc.css("div.feedable-details a")
+    @doc = Nokogiri::HTML(open("https://www.digitalocean.com/community/search?q=#{params["term"]}&type=tutorials"))
+    @articles = @doc.css("div.feedable-details a:not(.series)")
+    # Filter out questions with regex. The article['href'] part is what Nokogiri gives us
+    # Regex uses =~ and surrounds the regex with forward slashes
+    # @articles = @articles.reject { |article| article['href']=~/community\/questions\// }
+    # Use the binding.pry to stop the program so you can mess around in the terminal
+    # binding.pry
+    respond_to do |format|
+      format.html
+      format.json { render :json => @articles.to_json }
+    end
 #    puts articles
   end
 end
