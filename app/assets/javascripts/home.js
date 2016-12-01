25136d5e4715d20d9bb561b0d08691e7fa8f84da
Sharon C <captainshar@gmail.com>
2016-12-01 16:46:40 -0500

    Use the JSON from the community results and make it into HTML with JavaScript

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,19 +1,33 @@
 // Whenever any item with the name of topicsearch is submitted, alert hello
 // Use this document ready function to avoid the javascript loading too quickly
 $(document).ready(function() {
+	//the submit block is everything that happens when the search "Submit" form is submitted
 	$("#topicsearch").submit(function(event) {
+		// Getting the searchterm
+		// $("#searchterm").val() is the bit of text in the text box, using jQuery
+		var searchterm = $("#searchterm").val();
+		// This whole ajax block is to get the Community tutorial results
 		$.ajax({
 		    url: "api/community.json",
 		    type: "GET",
-		    // $("#searchterm").val() is the bit of text in the text box
 		    data: {
-		    	term: $("#searchterm").val()
+		    	term: searchterm
 		    }
-		  }).done(function(body) {
-		    $('#community-results').html(body);
+		  }).done(function(articles) {
+		  	var arrayLength = articles.length;
+				for (var i = 0; i < arrayLength; i++) {
+   				 //console.log(articles[i]);
+   				 $('#community-results').append("<div><a href=\""+articles[i].href+"\">"+articles[i].title+"</a></div>")
+   				 //TODO: Turn each element in the articles array into HTML and append to the
+   				 //community-results div on the index.html.erb page
+			}
+//		    $('#community-results').html(body);
+//		    console.log(articles);
 		  }).fail(function(xhr, status, errorThrown) {
 		  	$('#community-results').html('Something went wrong!');
 		  })
+		//Google Trends results fills in the iframe in the index.html.erb document
+    	document.getElementById("trendsFrame").src = "http://www.google.com/trends/fetchComponent?hl=en-US&q=" + searchterm + "&cmpt=q&content=1&cid=TIMESERIES_GRAPH_0&export=5&w=640&h=330";
 		event.preventDefault();
 	});
  });
--- app/controllers/api/communities_controller.rb
+++ app/controllers/api/communities_controller.rb
@@ -1,12 +1,15 @@
 class Api::CommunitiesController < ApplicationController
 
+  BASE_COMMUNITY_URL = "https://www.digitalocean.com"
+
   def show
   	# shows the search term
   	# with the url in the style http://localhost:3000/api/community.json?term=howdy
   	# TODO: Scrape the community page and return the correct JSON for the search, using Nokogiri
 #    render json: { term: params["term"] }
     require 'open-uri'
-    @doc = Nokogiri::HTML(open("https://www.digitalocean.com/community/search?q=#{params["term"]}&type=tutorials"))
+    
+    @doc = Nokogiri::HTML(open("#{BASE_COMMUNITY_URL}/community/search?q=#{params["term"]}&type=tutorials"))
     @articles = @doc.css("div.feedable-details a:not(.series)")
     # Filter out questions with regex. The article['href'] part is what Nokogiri gives us
     # Regex uses =~ and surrounds the regex with forward slashes
@@ -15,7 +18,14 @@ class Api::CommunitiesController < ApplicationController
     # binding.pry
     respond_to do |format|
       format.html
-      format.json { render :json => @articles.to_json }
+      format.json do 
+        # Pull out the Nokogiri elements we actually want and stuff them in a hash
+        articles_hash = @articles.map do |article|
+          {title: article.text, href: "#{BASE_COMMUNITY_URL}#{article['href']}" }
+        end
+        # Make the hash into JSON when the .json URL is called
+        render :json => articles_hash.to_json
+      end
     end
 #    puts articles
   end
